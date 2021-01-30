44d23345eabf0e9fa8d0b516d3bbb51c8b73cdc9
heathervu <78267411+heathervu@users.noreply.github.com>
2021-01-30 12:25:38 -0500

    window

--- LazyEggClicker.java
+++ LazyEggClicker.java
@@ -1,11 +1,16 @@
+import javax.swing.JFrame;
+
 class LazyEggClicker{
 	public static void main(String[] args) {
-		new LazyEggClicker()
+		new LazyEggClicker();
 	}
 	public LazyEggClicker() {
 		createUI();
 	}
 	public void createUI() {
+		JFrame window = new JFrame();
+		window.setSize(800,800);
 		
+		window.setVisible(true);
 	}
 }
